55518cb820472e2b6db71e0a9082c88ba0f1dbfc
Gabriel Barth <gabrielbarth@Gabriels-MacBook-Pro.local>
2022-10-16 14:31:21 -0400

    :heavy_plus_sign: adds tailwind dependency

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { Text, SafeAreaView, StyleSheet } from 'react-native'
 import { useSelector } from 'react-redux'
+import tw from 'tailwind-react-native-classnames'
 import { RootState } from '../../store'
 
 export default function Home() {
@@ -8,8 +9,16 @@ export default function Home() {
   console.log(reduxState)
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{reduxState.destination}</Text>
-    </View>
+    <SafeAreaView style={tw`bg-white h-full`}>
+      <Text style={tw`text-red-500 p-10`}>{reduxState.destination}</Text>
+    </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
